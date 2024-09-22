76c3b2bd992285c868df558351d79701ce5286c4
arfinorestualam <arfino.alam@gmail.com>
2024-09-22 17:10:24 +0700

    learn choose dependency

--- src/main/java/pzn/belajarspringdasar/DependencyInjectionConfiguration.java
+++ src/main/java/pzn/belajarspringdasar/DependencyInjectionConfiguration.java
@@ -1,15 +1,24 @@
 package pzn.belajarspringdasar;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import pzn.belajarspringdasar.data.Bar;
 import pzn.belajarspringdasar.data.Foo;
 import pzn.belajarspringdasar.data.FooBar;
 
 @Configuration
 public class DependencyInjectionConfiguration {
+    @Primary //digunakan agar tidak ada error, selalu memakai konfigurasi ini
+            //jika ditemukan konfigurasi kembar singkatnya default.
     @Bean
-    public Foo foo() {
+    public Foo fooFirst() {
+        return new Foo();
+    }
+
+    @Bean
+    public Foo fooSecond() {
         return new Foo();
     }
 
@@ -18,8 +27,10 @@ public class DependencyInjectionConfiguration {
         return new Bar();
     }
 
+    //Qualifier digunakan untuk memilih data yang digunakan, jika ada Primary
+    //Qualifier tetap di dahulukan
     @Bean
-    public FooBar fooBar(Foo foo, Bar bar) {
+    public FooBar fooBar(@Qualifier("fooSecond") Foo foo, Bar bar) {
         return new FooBar(foo, bar);
     }
 }
